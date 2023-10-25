11c53002fb8bedb08dfbe85212157c6a22a6df3a
rafael <rafaelcnunes@icloud.com>
2023-10-25 02:41:45 -0300

    add users module with crud

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,21 @@
+import { ConsoleLogger, Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalHttpExceptionFilter } from 'src/interceptors/global-http-exception-filter.interceptor';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [
+    UsersService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalHttpExceptionFilter,
+    },
+    ConsoleLogger,
+  ],
+})
+export class UsersModule {}
