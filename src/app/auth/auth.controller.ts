de9ed484522fe08ed419b46dd25194b27c428cc8
rafael <rafaelcnunes@icloud.com>
2023-10-28 02:18:26 -0300

    improvements at auth module

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,19 +1,32 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, HttpCode } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { AuthDto } from './dto/auth.dto';
+import { ApiResponse } from 'src/common/api-response';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @HttpCode(HttpStatus.CREATED)
   @Post('register')
-  register(@Body() registerDto: RegisterDto) {
-    return this.authService.register(registerDto);
+  async register(@Body() registerDto: RegisterDto): Promise<ApiResponse> {
+    return new ApiResponse(
+      true,
+      HttpStatus.CREATED,
+      'User registered successfully',
+      await this.authService.register(registerDto),
+    );
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
-  login(@Body() { email, password }: AuthDto) {
-    return this.authService.login(email, password);
+  async login(@Body() { email, password }: AuthDto): Promise<ApiResponse> {
+    return new ApiResponse(
+      true,
+      HttpStatus.OK,
+      'User logged in successfully',
+      await this.authService.login(email, password),
+    );
   }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { ConsoleLogger, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants/jwt-constants';
 import { UsersModule } from '../users/users.module';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalHttpExceptionFilter } from 'src/interceptors/global-http-exception-filter.interceptor';
 
 @Module({
   imports: [
@@ -19,6 +21,13 @@ import { UsersModule } from '../users/users.module';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalHttpExceptionFilter,
+    },
+    ConsoleLogger,
+  ],
 })
 export class AuthModule {}
